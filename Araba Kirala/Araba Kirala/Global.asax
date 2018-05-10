16c0579c9cfa24deb398c8d1a93a6df3ed3b04b8
Arife Kubra <arfekubra@gmail.com>
2018-05-10 13:31:26 +0300

    new project added

---
+++ Araba Kirala/Araba Kirala/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Araba_Kirala.MvcApplication" Language="C#" %>
---
+++ Araba Kirala/Araba Kirala/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Araba_Kirala.Startup))]
+namespace Araba_Kirala
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
